7363cf08dd7ae16962ebe86c200bf1d2c6ca70cc
bobmatyas <ballium@protonmail.com>
2019-11-19 18:01:18 -0500

    add desktop styles for boost component

--- src/components/boost.js
+++ src/components/boost.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import MobileBackgroundImage from '../images/bg-boost-mobile.svg';
+import DesktopBackgroundImage from '../images/bg-boost-desktop.svg';
 import ButtonRounded from '../components/button-rounded';
 
 
 const BoostSection = styled.section`
+  align-items: center;
   background-color: hsl(257, 27%, 26%); 
   background-image: url(${MobileBackgroundImage});
   background-position: top right;
   background-repeat: no-repeat;
-  padding: 8% 5%;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  justify-content: center;
+  padding: 0 5%;
   text-align: center;
+
+  @media only screen and (min-width: 376px) {
+    background-image: url(${DesktopBackgroundImage});
+    height: 250px;
+  }
 `;
 
 const BoostHeader = styled.h2`
   color: #fff;
   font-size: 2.6rem;
-  margin: 8rem auto 0 auto;
+  margin: 0 auto 0 auto;
   text-align: center;
 `;
 
 const ButtonHolder = styled.div`
-  margin-bottom: 6.8rem;
 `;
 
 function Boost() {
