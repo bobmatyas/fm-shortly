1b84fe83d0bcc0ce0ce4c57142834c1e895e9f4e
bobmatyas <ballium@protonmail.com>
2019-11-20 20:21:06 -0500

    add react copy clipboard package; style copy for desktop

--- src/components/shortener-result.js
+++ src/components/shortener-result.js
@@ -1,13 +1,26 @@
 import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
-
-
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+ 
 const fontSize = 1.6;
+const desktopSize = `700px`;
+
 
 const ShortenedLinkBox = styled.div`
+  align-items: center;
   background-color: #fff;
   border-radius: 5px;
+  display: flex;
+  flex-direction: column;
   margin: 1rem auto;
+
+  @media only screen and (min-width: ${desktopSize}) {
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 10px auto;
+    padding: 2.5%;
+  }
+
 `;
 
 const OriginalLink = styled.p`
@@ -16,9 +29,14 @@ const OriginalLink = styled.p`
   font-size: ${fontSize}rem;
   margin: 0;
   padding: 15px 15px 10px 15px;
+
+  @media only screen and (min-width: ${desktopSize}) {
+    border-bottom: none;
+    padding: 0;
+  }
 `;
 
-const ShortenedLink = styled.input`
+const ShortenedLink = styled.p`
   background-color: #fff;
   border: none;
   color: hsl(180, 66%, 49%);
@@ -27,8 +45,25 @@ const ShortenedLink = styled.input`
   padding: 15px;
 `;
 
+const CopyLinkHolder = styled.div`
+
+  margin: 0;
+  padding: 0;
+
+  @media only screen and (min-width: ${desktopSize}) {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+  }
+
+`;
+
 const CopyLinkButtonHolder = styled.div`
   padding-bottom: 15px;
+
+  @media only screen and (min-width: ${desktopSize}) {
+    padding: 0;
+  }
 `;
 
 const CopyLinkButton = styled.button`
@@ -44,27 +79,46 @@ const CopyLinkButton = styled.button`
   padding: 10px;
   text-align: center;
   width: 92%;
+
+  :active {
+    transform: translateY(4px);
+  }
+
+  :hover {
+    background-color: #9be3e2;
+    cursor: pointer;
+  }
+
+  @media only screen and (min-width: ${desktopSize}) {
+    padding: 8px 20px;
+    width: 100%;
+  }
 `;
 
 function ShortenerResult({ originalUrl, shortenedUrl }) {
   
   const [copySuccess, setCopySuccess] = useState('Copy');
   const shortenedRef = useRef(null);
+  const copyLink = useRef(null);
 
-  function copyToClipboard(e) {
-    shortenedRef.current.select();
-    document.execCommand('copy');
-    e.target.focus();
+  function copyToClipboard() {
     setCopySuccess('Copied!');
+    copyLink.current.classList.add('copied');
   };
 
   return (
     <ShortenedLinkBox>
       <OriginalLink>{ originalUrl }</OriginalLink>
-      <ShortenedLink ref={shortenedRef} readOnly value={shortenedUrl} disabled></ShortenedLink>
+      <CopyLinkHolder>
+      <ShortenedLink ref={shortenedRef}>{shortenedUrl}</ShortenedLink>
       <CopyLinkButtonHolder>
-        <CopyLinkButton onClick={copyToClipboard}>{copySuccess}</CopyLinkButton>
+      <CopyToClipboard 
+        text={shortenedUrl}
+        onCopy={() => copyToClipboard()}>
+        <CopyLinkButton ref={copyLink}>{copySuccess}</CopyLinkButton>
+      </CopyToClipboard>
       </CopyLinkButtonHolder>
+      </CopyLinkHolder>
     </ShortenedLinkBox> 
   );
 }
