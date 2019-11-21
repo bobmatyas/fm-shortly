0f4de643e344847fb4a9a803d4f2c7ae27e46cc9
bobmatyas <ballium@protonmail.com>
2019-11-21 11:47:53 -0500

    Updates

---
+++ precache-manifest.8667362cf0453febcafda762308e39b1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e1461cf67d7433d5336ef906ac1a8c",
+    "url": "/fm-shortly/index.html"
+  },
+  {
+    "revision": "9d5c72ee313035efafea",
+    "url": "/fm-shortly/static/css/main.6f18f5ad.chunk.css"
+  },
+  {
+    "revision": "d236679a7f6d92f35818",
+    "url": "/fm-shortly/static/js/2.ad19cf0f.chunk.js"
+  },
+  {
+    "revision": "9d5c72ee313035efafea",
+    "url": "/fm-shortly/static/js/main.4dece6aa.chunk.js"
+  },
+  {
+    "revision": "0f8c30f67a7002861cb3",
+    "url": "/fm-shortly/static/js/runtime-main.5d27c742.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/fm-shortly/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/fm-shortly/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/fm-shortly/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/fm-shortly/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/fm-shortly/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/fm-shortly/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "8d36318a93b26e227a414811072a87ba",
+    "url": "/fm-shortly/static/media/icon-facebook.8d36318a.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/fm-shortly/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "f00517c8ac0acaaefd24022a4b144f52",
+    "url": "/fm-shortly/static/media/icon-instagram.f00517c8.svg"
+  },
+  {
+    "revision": "7bfcc413cf813cf84021ce853e28011e",
+    "url": "/fm-shortly/static/media/icon-pinterest.7bfcc413.svg"
+  },
+  {
+    "revision": "ce8e482b83c41ca1b7720dbd710fe975",
+    "url": "/fm-shortly/static/media/icon-twitter.ce8e482b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/fm-shortly/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "eb2fe952dc763c2d9d0a83041575c087",
+    "url": "/fm-shortly/static/media/logo.eb2fe952.svg"
+  }
+]);
